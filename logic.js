f47773823dd68f663b6eab6a0cd76fb7f314681b
Arthur.Lee <arthur.lee.ac72@gmail.com>
2017-03-31 13:32:30 +0800

    update

--- logic.js
+++ logic.js
@@ -5,31 +5,34 @@ function active() {
 }
 
 function click_logic(node, config) {
+	node.mouseenter();
 	var target_widw = document.querySelector("#widw");
 	var observer_widwnext = new MutationObserver(function () {
 		var handler = self.setInterval(function () {
 			if ($(".contentItems") != null) {
 				observer_widwnext.disconnect();
-				$("#widw").css("display", "none");
-				$("#bg-widw").css("display", "none");
+				$("widw-close").click();
 				window.clearInterval(handler);
 			}
 		}, 40);
+		node.append("<div class='show-rawext'>" + node.attr("data-rawext").substr(0, 10) + "</div>");
 		click_logic(node.next(), config);
 	});
 	var observer_widw = new MutationObserver(function () {
-		observer_widwnext.observe(target_widw, config);
+		//observer_widwnext.observe(target_widw, config);
+		$(".txtInspect").attr("val", inspect);
 		setTimeout(function () {
 			$(".btnCheck").click();
 		}, 50);
 		observer_widw.disconnect();
 	});
-	node.mouseenter();
 	if (node.hasClass("Key") || node.hasClass("Sticker") || node.hasClass("Container") || node.hasClass("Tool") || node.hasClass("Graffiti") || node.hasClass("Collectible") || node.hasClass("Music")) {
 		click_logic(node.next(), config)
 	} else {
+		var inspect = $(".ingame.OptionInspect").attr("href");
 		if (node.attr("data-rawext") > 1) {
 			observer_widw.observe(target_widw, config);
+			alert(inspect);
 			document.querySelector("a[href^='#item']").click();
 		} else {
 			node.append("<div class='show-rawext'>" + node.attr("data-rawext").substr(0, 10) + "</div>");
