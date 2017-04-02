06be2363937074e80e8d7f7b452dfc2a2b46f257
Luna <arthu.lee.ac72@outlook.com>
2017-04-02 20:39:21 +0800

    update

--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
-	var logic = chrome.runtime.getURL("logic.js");
-	$("script:first").replaceWith("<script src='//cdn.bootcss.com/jquery/3.2.1/jquery.min.js'></script>");
-	$("head").append("<script src=" + logic + "></script>");
-	$(".contentMain").before("<div class='bar'><a href='#' onclick='active()'>显示磨损</a></div>");
+    var logic = chrome.runtime.getURL("logic.js");
+    var addon = chrome.runtime.getURL("addon.js");
+    $("script:first").replaceWith("<script src='//cdn.bootcss.com/jquery/3.2.1/jquery.min.js'></script>");
+    $("head").append("<script src=" + logic + "></script>");
+    $("head").append("<script src=" + addon + "></script>");
+    $(".contentMain").before("<div id='showExterior' class='bar'><a href='#' onclick='active()'>显示磨损</a></div>");
+    $("#showExterior").one("click", active());
 });
 
 
