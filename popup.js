16a1f7a6a9f53ed473b9dc25fd3a6352b907690a
Arthur.Lee <arthur.lee.ac72@gmail.com>
2017-04-06 19:35:05 +0800

    0.9 Beta

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
     var logic = chrome.runtime.getURL("logic.js");
-    var addon = chrome.runtime.getURL("addon.js");
     $("script:first").replaceWith("<script src='//cdn.bootcss.com/jquery/3.2.1/jquery.min.js'></script>");
     $("head").append("<script src=" + logic + "></script>");
-    $("head").append("<script src=" + addon + "></script>");
     $(".contentMain").before("<div id='showExterior' class='bar'><a href='#' onclick='active()'>查询磨损(Retrieve)</a></div>");
 });
 
