5f14132e8a18c84833bb89c3f1afc2443ebe4d64
Arthur.Lee <arthur.lee.ac72@gmail.com>
2017-04-08 13:16:29 +0800

    2.0 Release

--- logic.js
+++ logic.js
@@ -13,7 +13,7 @@ let inspectCache = "";
 //以上为对象缓存
 const configSubtree = {attributes: true, childList: true, characterData: false, subtree: true};
 //以上为Config对象
-let observerCheck = new MutationObserver(function () { //查询地址填充与check按钮点击监听器
+let observerCheck = new MutationObserver(function () { //查询地址填充与查询请求提交监听器
 	if ($widw.css("display") === "block") {
 		observerCheck.disconnect();
 		$(".txtInspect").attr("value", inspectCache);
@@ -22,23 +22,26 @@ let observerCheck = new MutationObserver(function () { //查询地址填充与ch
 });
 //以上为被动监听器对象
 
-$(document).ready(function () {
+$(document).ready(function () { //检测页面是否被脚本刷新
 	if (sessionStorage.AutoclickRefreshCount && Number(sessionStorage.AutoclickRefreshCount) > 0) {
-		if (Number(sessionStorage.AutoclickRefreshCount) < 4) {
+		if (Number(sessionStorage.AutoclickRefreshCount) < 5) {
+			$("title").text("脚本运行中-已刷新...");
 			let handler = window.setInterval(function () {
 				if ($(".statsInv").text() !== "") {
 					window.clearInterval(handler);
 					clickLogic($(".vItem.cItem:first"));
 				}
 			}, 50);
-		} else if (sessionStorage.AutoclickRefreshCount === 5) {
+		} else if (sessionStorage.AutoclickRefreshCount === 6) {
 			onceFlag = false;
 		} else {
+			$("title").text("已完成");
 			let handler = window.setInterval(function () {
 				if ($(".statsInv").text() !== "") {
+					onceFlag = false;
 					finishedFlag = true;
 					Notification.requestPermission(function () {
-						let n = new Notification("Task Finished");
+						let n = new Notification("已完成");
 					});
 					window.clearInterval(handler);
 					clickLogic($(".vItem.cItem:first"));
@@ -50,22 +53,23 @@ $(document).ready(function () {
 	}
 });
 
-function active() {
+function active() { //启动脚本
 	finishedFlag = false;
 	sessionStorage.AutoclickRefreshCount = 0;
 	if (onceFlag) {
 		onceFlag = false;
+		$("title").text("脚本运行中...");
 		clickLogic($(".vItem.cItem:first"));
 	}
 }
 
-function clickLogic(node) {
-	let observerClose = new MutationObserver(function () { //关闭按钮点击监听器
+function clickLogic(node) { //脚本主体
+	let observerClose = new MutationObserver(function () { //查询窗口关闭监听器
 		if ($(".detailItem") !== null || $(".contentWeb") !== null) {
 			observerClose.disconnect();
 			$widw.css("display", "none");
 			$bgwidw.css("display", "none");
-			node.append("<div class='show-rawext'>success</div>");
+			node.append("<div class='show-rawext'>retrieved</div>");
 			setTimeout(function () {
 				controller(node, reloadFlag);
 			}, 1000);
@@ -88,11 +92,11 @@ function clickLogic(node) {
 	}
 }
 
-function controller(node, reloadFlag) {
+function controller(node, reloadFlag) { //循环控制器
 	if (node.next().hasClass("vItem")) {
 		clickLogic(node.next());
 	} else {
-		if (reloadFlag === true && Number(sessionStorage.AutoclickRefreshCount) < 4) {
+		if (reloadFlag === true && Number(sessionStorage.AutoclickRefreshCount) < 5) {
 			sessionStorage.AutoclickRefreshCount = Number(sessionStorage.AutoclickRefreshCount) + 1;
 			location.reload(true);
 		} else {
@@ -100,20 +104,21 @@ function controller(node, reloadFlag) {
 			} else {
 				onceFlag = false;
 				observerCheck.disconnect();
-				sessionStorage.AutoclickRefreshCount = 5;
+				sessionStorage.AutoclickRefreshCount = 6;
+				$("title").text("已完成");
 				Notification.requestPermission(function () {
-					let n = new Notification("Task Finished");
+					let n = new Notification("已完成");
 				});
 			}
 		}
 	}
 }
 
-function inspect(node) {
+function inspect(node) { //返回查询链接
 	return "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S" + node.data("profile") + "A" + node.data("id") + "D" + node.data("inspect");
 }
 
-function parseInspect() {
+function parseInspect() { //提交查询请求
 	let t = $(".txtInspect").val(),
 		e = /steam:\/\/rungame\/730\/\d*?\/\+csgo_econ_action_preview( |%20)(M|S)(\d*)[A](\d*)[D](\d*)/gm;
 	null !== (m = e.exec(t)) ? null != m[2] && null != m[3] && null != m[4] && null != m[5] ? ($("#MarketFloatValue").html("<img class='loading' src='/images/loader.gif' alt='loading...' />"), $("#MarketFloatValue").load("/item/float", {
